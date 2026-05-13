e21c41c1c8536020e25f88d279ae0ba318481d88
CodeHanaa <bhanajy044@gmail.com>
2026-05-13 21:02:00 +0300

    add tailwind config and setup project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          1: "#0D0D0D",
+          2: "#1A1A1A",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
