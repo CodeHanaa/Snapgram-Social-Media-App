5208439c9e2aadd7d28ad0bd6bc7dff1edb4b130
CodeHanaa <bhanajy044@gmail.com>
2026-06-08 22:06:51 +0300

    feat: add QueryProvider and fix AuthProvider context and signin logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from '@/Context/AuthProvider';
+import { QueryProvider } from './lib/react-query/QueryProvider';
+import './index.css';
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
